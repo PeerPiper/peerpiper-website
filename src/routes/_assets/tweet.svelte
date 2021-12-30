e9cedd9e5da401910815801fae365a8967b8508e
Doug <douganderson444@gmail.com>
2021-12-30 11:33:45 -0400

    beef up guide, add tweet

---
+++ src/routes/_assets/tweet.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</svelte:head>
+<center>
+	<blockquote class="twitter-tweet">
+		<p lang="en" dir="ltr">
+			Excited for Web3 to be mass adopted, here&#39;s a colorful comparison summary for the normies. <a
+				href="https://t.co/VKCEtxPBmt">pic.twitter.com/VKCEtxPBmt</a
+			>
+		</p>
+		&mdash; D◎ug Anderson 🦀👨🏼‍💻 (@DougAnderson444)<a
+			href="https://twitter.com/DougAnderson444/status/1468623301954129923?ref_src=twsrc%5Etfw"
+			>December 8, 2021</a
+		>
+	</blockquote>
+</center>
+
+<style>
+	center {
+		text-align: center;
+	}
+</style>
