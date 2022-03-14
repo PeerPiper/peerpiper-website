b4e8f9ec4012a3589ba283faf886f4db57bd6247
Doug A <douganderson444@gmail.com>
2022-03-14 15:01:07 -0300

    add wallet subdomain option

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,45 +3,18 @@
 </script>
 
 <script lang="ts">
-	import PGraphic from '$lib/graphics/p.svelte';
-	import PeerStream from '$lib/graphics/PeerStream.svelte';
+	import Home from '$lib/Home.svelte';
+
+	import { page } from '$app/stores';
+
+	import { onMount } from 'svelte';
+
+	let dynamicComponent = Home;
+
+	// TODO: Wallet subdomain
+	// if ($page.url.hostname.startsWith('wallet.')) dynamicComponent = Wallet;
+
+	onMount(() => {});
 </script>
 
-<svelte:head>
-	<title>PeerPiper.io</title>
-</svelte:head>
-
-<div class="hero">
-	<PGraphic />
-	<h1 class="title">The easiest way to automatically stay in touch.</h1>
-</div>
-<section>
-	<PeerStream />
-	<p class="description">
-		Save anything once, pipe updates to your peers from the privacy of your own devices.
-	</p>
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-		font-weight: bold;
-	}
-
-	.hero {
-		margin: 30px 0px;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-		color: #525252;
-		width: 100%;
-		text-align: center;
-	}
-</style>
+<svelte:component this={dynamicComponent} />
