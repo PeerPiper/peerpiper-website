3f7ea31a18a6652f8aed2e4cbd774a5fd511081a
Doug Anderson444 <douganderson444@gmail.com>
2024-02-07 14:36:56 -0400

    add graphic

---
+++ src/lib/graphics/protocol.svelte
@@ -0,0 +1,8 @@
+<script>
+	import protocol from '$lib/graphics/protocol.svg?raw';
+</script>
+
+<!-- Use flexbox to keep the maximum width of the img to the width of the screen viewport -->
+<div style="display: flex; justify-content: center;">
+	{@html protocol}
+</div>
