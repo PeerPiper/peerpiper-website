1043d873b26a64ac81e3ffbf7465c5db73523ace
Doug Anderson444 <douganderson444@gmail.com>
2025-02-02 10:02:16 -0400

    add provenance-log to paper

---
+++ src/lib/graphics/provenance-log.svelte
@@ -0,0 +1,8 @@
+<script>
+	import history from '$lib/graphics/key-history.jpeg';
+</script>
+
+<!-- Use flexbox to keep the maximum width of the img to the width of the screen viewport -->
+<div style="display: flex; justify-content: center;">
+    <img src={history} alt="Key History" style="max-width: 100%;" />
+</div>
